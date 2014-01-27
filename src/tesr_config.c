6b160f201878ea52f0d77a8f3d10e1dd3dbb70a6
Tuenti <tuenti@ubuntu-wijnandTemp.(none)>
2014-01-27 13:16:09 +0100

    fix the configuration so it is compiling on ubuntu linux

--- src/tesr_config.c
+++ src/tesr_config.c
@@ -61,7 +61,7 @@ void init_config(tesr_config_t *tesr_config, int argc, char **argv) {
     // Configuration File
     //
     config_t cfg;
-    char *local_config_file_name = "tesr.conf";
+    char *local_config_file_name = "./tesr.conf";
     char *system_config_file_name = "/etc/tesr.conf";
     //Initialization
     config_init(&cfg);
@@ -69,75 +69,85 @@ void init_config(tesr_config_t *tesr_config, int argc, char **argv) {
     /* Read the file. If there is an error, report it and exit. */
     if(!config_loaded) {
         config_loaded = config_read_file(&cfg, local_config_file_name);
-        if(config_loaded) LOG_INFO("using config ./%s\n", local_config_file_name);
+        if(config_loaded) {
+		LOG_INFO("using config %s\n", local_config_file_name);
+	} else {
+		LOG_INFO("no local config found at %s\n", local_config_file_name);
+	}
     }
     if(!config_loaded) {
         config_loaded = config_read_file(&cfg, system_config_file_name);
-        if(config_loaded) LOG_INFO("using config %s\n", system_config_file_name);
+        if(config_loaded) {
+		LOG_INFO("using config %s\n", system_config_file_name);
+	} else {
+		LOG_INFO("no system config found at %s\n", system_config_file_name);
+	}
     }
     if(!config_loaded) {
         LOG_WARN("\n%s:%d - %s", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
         config_destroy(&cfg);
     }
-    // get the value of recv_port
-    if(config_lookup_int(&cfg, "recv_port", &configPort)) {
-        LOG_DEBUG("\nrecv_port: %d\n", configPort);
-        tesr_config->recv_port = configPort;
-    } else {
-        LOG_DEBUG("\nNo 'recv_port' setting in configuration file.");
-    }
-    // get the value of ip_rate_limit_max
-    if(config_lookup_int(&cfg, "ip_rate_limit_max", &configPort)) {
-        LOG_INFO("\nip_rate_limit_max: %d\n", configPort);
-        tesr_config->ip_rate_limit_max = configPort;
-    } else {
-        LOG_INFO("\nNo 'ip_rate_limit_max' setting in configuration file disabling.");
-    }
-    // get the value of ip_rate_limit_period
-    if(config_lookup_int(&cfg, "ip_rate_limit_period", &configPort)) {
-        LOG_INFO("\nip_rate_limit_period: %d\n", configPort);
-        tesr_config->ip_rate_limit_period = configPort;
-    } else {
-        LOG_INFO("\nNo 'ip_rate_limit_period' setting in configuration file.");
-    }
-    // get the value of ip_rate_limit_prune_mark
-    if(config_lookup_int(&cfg, "ip_rate_limit_prune_mark", &configPort)) {
-        LOG_INFO("\nip_rate_limit_period: %d\n", configPort);
-        tesr_config->ip_rate_limit_prune_mark = configPort;
-    } else {
-        LOG_INFO("\nNo 'ip_rate_limit_prune_mark' setting in configuration file.");
-    }
-    //get the value of send_ports 
-    config_setting_t *send_ports = config_lookup(&cfg, "send_ports");
-    int send_port_idx = 0;
-    if(send_ports) {
-        tesr_config->num_worker_threads = 0;
-        config_setting_t *send_port = config_setting_get_elem(send_ports, send_port_idx++);
-        while(send_port) {
-            tesr_send_port_t *element = (tesr_send_port_t *)malloc(sizeof(tesr_send_port_t));
-            element->port = config_setting_get_int(send_port);
-            LL_PREPEND(tesr_config->send_ports, element);
-            LOG_DEBUG("send_port=%d\n", element->port);
-            send_port = config_setting_get_elem(send_ports, send_port_idx++);
-            ++tesr_config->num_worker_threads;
-        }
-    }
-    config_setting_t *filters = config_lookup(&cfg, "filters");
-    int filterIdx = 0;
-    if(filters) {
-        LOG_DEBUG("we have filters\n");
-        config_setting_t *filter = config_setting_get_elem(filters, filterIdx++);
-        while(filter) {
-            LOG_DEBUG("we have a filter\n");
-            const char *sz_filter = config_setting_get_string(filter);
-            if(sz_filter) {
-                tesr_filter_t *element = (tesr_filter_t *)malloc(sizeof(tesr_filter_t));
-                strncpy(element->filter, sz_filter, INET_ADDRSTRLEN);
-                LL_PREPEND(tesr_config->filters, element);
-                LOG_DEBUG("filter=%s\n", sz_filter);
-            }
-            filter = config_setting_get_elem(filters, filterIdx++);
-        }
+    if(config_loaded) {
+    	// get the value of recv_port
+    	if(config_lookup_int(&cfg, "recv_port", &configPort)) {
+    	    LOG_DEBUG("\nrecv_port: %d\n", configPort);
+    	    tesr_config->recv_port = configPort;
+    	} else {
+    	    LOG_DEBUG("\nNo 'recv_port' setting in configuration file.");
+    	}
+    	// get the value of ip_rate_limit_max
+    	if(config_lookup_int(&cfg, "ip_rate_limit_max", &tesr_config->ip_rate_limit_max)) {
+    	    LOG_INFO("\nip_rate_limit_max: %d\n", tesr_config->ip_rate_limit_max);
+    	    tesr_config->ip_rate_limit_max = tesr_config->ip_rate_limit_max;
+    	} else {
+    	    LOG_INFO("\nNo 'ip_rate_limit_max' setting in configuration file disabling.");
+    	}
+    	// get the value of ip_rate_limit_period
+    	if(config_lookup_int(&cfg, "ip_rate_limit_period", &tesr_config->ip_rate_limit_period)) {
+    	    LOG_INFO("\nip_rate_limit_period: %d\n", tesr_config->ip_rate_limit_period);
+    	    tesr_config->ip_rate_limit_period = tesr_config->ip_rate_limit_period;
+    	} else {
+    	    LOG_INFO("\nNo 'ip_rate_limit_period' setting in configuration file.");
+    	}
+    	// get the value of ip_rate_limit_prune_mark
+    	if(config_lookup_int(&cfg, "ip_rate_limit_prune_mark", &tesr_config->ip_rate_limit_prune_mark)) {
+    	    LOG_INFO("\nip_rate_limit_period: %d\n", tesr_config->ip_rate_limit_prune_mark);
+    	    tesr_config->ip_rate_limit_prune_mark = tesr_config->ip_rate_limit_prune_mark;
+    	} else {
+    	    LOG_INFO("\nNo 'ip_rate_limit_prune_mark' setting in configuration file.");
+    	}
+    	//get the value of send_ports 
+    	config_setting_t *send_ports = config_lookup(&cfg, "send_ports");
+    	int send_port_idx = 0;
+    	if(send_ports) {
+    	    tesr_config->num_worker_threads = 0;
+    	    config_setting_t *send_port = config_setting_get_elem(send_ports, send_port_idx++);
+    	    while(send_port) {
+    	        tesr_send_port_t *element = (tesr_send_port_t *)malloc(sizeof(tesr_send_port_t));
+    	        element->port = config_setting_get_int(send_port);
+    	        LL_PREPEND(tesr_config->send_ports, element);
+    	        LOG_DEBUG("send_port=%d\n", element->port);
+    	        send_port = config_setting_get_elem(send_ports, send_port_idx++);
+    	        ++tesr_config->num_worker_threads;
+    	    }
+    	}
+    	config_setting_t *filters = config_lookup(&cfg, "filters");
+    	int filterIdx = 0;
+    	if(filters) {
+    	    LOG_DEBUG("we have filters\n");
+    	    config_setting_t *filter = config_setting_get_elem(filters, filterIdx++);
+    	    while(filter) {
+    	        LOG_DEBUG("we have a filter\n");
+    	        const char *sz_filter = config_setting_get_string(filter);
+    	        if(sz_filter) {
+    	            tesr_filter_t *element = (tesr_filter_t *)malloc(sizeof(tesr_filter_t));
+    	            strncpy(element->filter, sz_filter, INET_ADDRSTRLEN);
+    	            LL_PREPEND(tesr_config->filters, element);
+    	            LOG_DEBUG("filter=%s\n", sz_filter);
+    	        }
+    	        filter = config_setting_get_elem(filters, filterIdx++);
+    	    }
+    	}
     }
     //
     // OptArg Overrides
